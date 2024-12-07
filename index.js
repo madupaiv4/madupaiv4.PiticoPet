cd0788a6386ef4bb729ad0d1390dd72e681878a6
Madu <eduardapaiva2008@gmail.com>
2024-12-07 17:39:58 -0300

    .

---
+++ index.js
@@ -0,0 +1,8 @@
+Swal.fire({
+    title: "Aponte sua câmera!",
+    text: "Link do site :D",
+    imageUrl: "imagens/qr-code.jpg",
+    imageWidth: 200,
+    imageHeight: 200,
+    imageAlt: "QR-code image"
+  });
