1526a643ac2571753e15d5a3d3587652f3eccf02
Madu <eduardapaiva2008@gmail.com>
2024-12-08 23:36:16 -0300

    .

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-Swal.fire({
- title: "Aponte sua câmera!",
- text: "Link do site :D",
- imageUrl: "imagens/qr-code.jpg",
- imageWidth: 200,
- imageHeight: 200,
- imageAlt: "QR-code image"
- });
+
 
 let index = 0;
 
--- layout.js
+++ layout.js
@@ -8,7 +8,7 @@ function alteraSidebar() {
     console.log(showSidebar);
     if (showSidebar) {
         navHeader.style.marginLeft = '-9vw';
-        navHeader.style.animationName = 'showSiderbar';
+        navHeader.style.animationName = 'showSidebar';
         content.style.filter = 'blur(1px)';
     } else {
         navHeader.style.marginLeft = '-100vw';
@@ -17,18 +17,32 @@ function alteraSidebar() {
     }
 }
 
-function fecharSidebar()
-{
-    if (showSidebar)
-    {
+function fecharSidebar() {
+    if (showSidebar) {
         alteraSidebar();
     }
 }
 
 window.addEventListener('resize', function(event) {
-    if(window.innerWidth > 768 && showSidebar) 
-    {  
-        showSidebar = true;
+    if (window.innerWidth > 768 && showSidebar) {  
+        showSidebar = false;
         alteraSidebar();
     }
 });
+
+const button = document.getElementById('bnt-qrcode');
+
+button.addEventListener('click', () => {
+    Swal.fire({
+        title: "Aponte sua câmera!",
+        text: "Link do site :D",
+        imageUrl: "imagens/qr-code.jpg",
+        imageWidth: 200,
+        imageHeight: 200,
+        imageAlt: "QR-code image",
+        confirmButtonText: "Fechar",
+        })
+});
+
+
+
